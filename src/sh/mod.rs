2495555976cc5f444a8f51f4001f12da24ffa69d
Alexandru Radovici <msg4alex@gmail.com>
2019-12-07 18:06:55 +0200

    sh command line

--- src/sh/mod.rs
+++ src/sh/mod.rs
@@ -8,21 +8,18 @@ use sh_ast::*;
 use super::command;
 
 command! ("sh", "The shell", execute, 
-	
+	(short="c", help="run command")
+	command: Vec<String>
 );
 
 pub fn execute (options: Options) -> Result<(), io::Error>
 {
 	let mut errno = 0;
 
-	let mut redirects: Redirects = Redirects::new ();
-
-    let command = sh_rules::CommandParser::new().parse(&mut redirects, "p1>3 | p2 | s ; E= s3>$12>4 s2");
-
-	match command {
-        Ok (s) => println! ("{:#?}", s),
-        Err (e) => eprintln! ("{}", e)
-    };
+	if options.command.len() > 0 {
+		let command_str = options.command.join (" ");
+		parse_command (&options, &command_str);
+	}
 
 	if errno != 0 {
 		Err (io::Error::from_raw_os_error (errno))
@@ -31,4 +28,15 @@ pub fn execute (options: Options) -> Result<(), io::Error>
 	{
 		Ok (())
 	}
+}
+
+fn parse_command (options: &Options, command_str:&str) -> Result<(), io::Error>
+{
+	let mut redirects: Redirects = Redirects::new ();
+	let command = sh_rules::CommandParser::new().parse(&mut redirects, command_str);
+	match command {
+		Ok (s) => println! ("{:#?}", s),
+		Err (e) => eprintln! ("{}", e)
+	};
+	Ok (())
 }
